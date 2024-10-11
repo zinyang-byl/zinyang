7f96ab929de9f3bc291d54f00456fcb148772391
zinyang-byl <shakaai014@gmail.com>
2024-10-11 09:49:39 +0700

    Add files via upload

--- vendors/swiper/types/index.d.ts
+++ vendors/swiper/types/index.d.ts
@@ -1,6 +1,53 @@
-// @ts-nocheck
-export * from './shared.d.ts';
-export { default as Swiper } from './swiper-class.d.ts';
-export * from './swiper-events.d.ts';
-export * from './swiper-options.d.ts';
-export * from './modules/public-api.d.ts';
+import type { SwiperModule } from '../shared.d.ts';
+
+declare const A11y: SwiperModule;
+declare const Autoplay: SwiperModule;
+declare const Controller: SwiperModule;
+declare const EffectCoverflow: SwiperModule;
+declare const EffectCube: SwiperModule;
+declare const EffectFade: SwiperModule;
+declare const EffectFlip: SwiperModule;
+declare const EffectCreative: SwiperModule;
+declare const EffectCards: SwiperModule;
+declare const HashNavigation: SwiperModule;
+declare const History: SwiperModule;
+declare const Keyboard: SwiperModule;
+declare const Lazy: SwiperModule;
+declare const Mousewheel: SwiperModule;
+declare const Navigation: SwiperModule;
+declare const Pagination: SwiperModule;
+declare const Parallax: SwiperModule;
+declare const Scrollbar: SwiperModule;
+declare const Thumbs: SwiperModule;
+declare const Virtual: SwiperModule;
+declare const Zoom: SwiperModule;
+declare const FreeMode: SwiperModule;
+declare const Grid: SwiperModule;
+declare const Manipulation: SwiperModule;
+
+export {
+  A11y,
+  Autoplay,
+  Controller,
+  EffectCoverflow,
+  EffectCube,
+  EffectFade,
+  EffectFlip,
+  EffectCreative,
+  EffectCards,
+  HashNavigation,
+  History,
+  Keyboard,
+  Lazy,
+  Mousewheel,
+  Navigation,
+  Pagination,
+  Parallax,
+  Scrollbar,
+  Thumbs,
+  Virtual,
+  Zoom,
+  FreeMode,
+  Grid,
+  Manipulation,
+};
